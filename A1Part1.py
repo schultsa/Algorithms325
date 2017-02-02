946b135db5ce22ff64f6b1f3321fe1cf42b8d8be
erinsullens <sullense@oregonstate.edu>
2017-02-01 20:49:46 -0800

    almost finished
    part 1 having problems, but other two are done, except for getting command args to work for file input

---
+++ A1Part1.py
@@ -0,0 +1,95 @@
+import random
+import sys
+import os
+import math
+
+
+############################################################
+# This section is for putting the numbers from the file into an array
+############################################################
+class Point:
+    x = 0;
+    y = 0;
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+def distance(pointa, pointb):
+    dis = math.sqrt((pointa.x-pointb.x)**2+(pointa.y-pointb.y)**2)
+    return dis
+
+def printArray(array):
+    for n in range(0, len(array)):
+        print(array[n].x, array[n].y)
+file = open("example.input", "r+")
+text = file.read()
+numberOfPairs = text.count('\n')
+array = []
+
+i = 0
+x = float
+y = float
+xstring = ""
+ystring = ""
+
+num = 0
+xcoor = True
+
+while(num<len(text)):
+    if((text[num].isdigit() or text[num] == '.') and xcoor):
+        xstring+=(text[num])
+        if(text[num+1] == ' ' or text[num+1] == '\n'):
+            xcoor = False
+            x = float(xstring)
+    else:
+        if ((text[num].isdigit() or text[num] == '.') and not(xcoor)):
+            ystring+=(text[num])
+            if (text[num+1] == '\n' or text[num+1]==' '):
+                xcoor = True
+                y = float(ystring)
+                array.append(Point(x,y))
+                xstring = ""
+                ystring = ""
+
+    num += 1
+
+
+############################################################
+# This section is the algorithm
+############################################################
+
+m = 1000000000000.0
+dis = float
+arrayOfMins = []
+
+for f in range(0, numberOfPairs):
+    for s in range(f, numberOfPairs):
+        if(not(f ==s)):
+            dis = math.sqrt((array[f].x-array[s].x)**2+(array[f].y-array[s].y)**2)
+            if dis == m:
+                arrayOfMins.append(array[f])
+                arrayOfMins.append(array[s])
+            if dis < m:
+                arrayOfMins[:] = []
+                arrayOfMins.append(array[f])
+                arrayOfMins.append(array[s])
+                m = distance
+
+file = open("BruteForce_output.txt","w")
+t = distance(arrayOfMins[0], arrayOfMins[1])
+distance1 = str(t)
+distance1+='\n'
+file.write(distance1)
+for n in range(0, len(arrayOfMins)):
+    stringToWrite = str(arrayOfMins[n].x)
+    stringToWrite+= " "
+    stringToWrite+= str(arrayOfMins[n].y)
+    stringToWrite+='\n'
+    file.write(stringToWrite)
+
+print(distance(arrayOfMins[0], arrayOfMins[1]))
+
+for n in range(0, len(arrayOfMins)):
+    print(arrayOfMins[n].x, arrayOfMins[n].y)
+
+
--- A1p3.py
+++ A1p3.py
@@ -9,22 +9,21 @@ import math
 # This section is for putting the numbers from the file into an array
 ############################################################
 class Point:
-    x = 0;
-    y = 0;
+    x = float;
+    y = float;
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-
-file = open(sys.argv[1], "r+")
-text = file.read()
-numberOfPairs = text.count('\n')
-
-array = []
 def printArray(array):
     for n in range(0, len(array)):
         print(array[n].x, array[n].y)
+file = open("numbers.input", "r+")
+text = file.read()
+numberOfPairs = text.count('\n')
+#print(numberOfPairs)
+array = []
 
 i = 0
 x = float
@@ -53,6 +52,30 @@ while(num<len(text)):
 
     num += 1
 
+#printArray(array)
+# for m in range(0, numberOfPairs):
+#     if (text[(i+1)] == ' ') and (text[i].isdigit()):
+#         x = int(text[i])
+#         i +=1
+#
+#     if (text[i] == ' '):
+#         i += 1
+#     if (text[i].isdigit()):
+#         y = int(text[i])
+#         i += 1
+#
+#
+#     if(i<len(text)):
+#         if (text[i] == ' '):
+#             i += 1
+#     if (i < len(text)):
+#         if(text[i] == '\n'):
+#             i+=1
+#     if (i < len(text)):
+#         if (text[i] == ' '):
+#             i += 1
+#
+#     array.append(Point(x,y))
 # print("Original Array:\n")
 # for n in range(0, numberOfPairs):
 #     print(array[n].x, array[n].y)
@@ -165,51 +188,49 @@ def combine (left, right, sortBy):
 ############################################################
 # This section is the find closest pair algorithm
 ############################################################
+def containsPair(array, pointa, pointb):
+    for i in range(0, len(array)):
+        if((array[i].x == pointa.x and array[i].y == pointa.y) and (array[i+1].x== pointb.x) and(array[i+1].y== pointb.y)):
+            return True
+        else:
 
+            if ((array[i].x == pointb.x and array[i].y == pointb.y) and (array[i + 1].x == pointa.x) and (
+                array[i + 1].y == pointa.y)):
+                return True
+        i+=1
 
+    return False
 def distance(pointa, pointb):
     dis = math.sqrt((pointa.x-pointb.x)**2+(pointa.y-pointb.y)**2)
     return dis
 
-def find_M_andComputeSmallestPair(left, right, min):
-    L = (left[len(left)-1].x + right[0].x)/2
+def find_M_andComputeSmallestPair(leftx, rightx, min, arrayy):
+    L = (leftx[len(leftx)-1].x + rightx[0].x)/2
     M = []
     beta = distance(min[0], min[1])
-    leftside = True
-    rightside = True
-    counter = len(left)-1
-    while (leftside == True):
-        if abs(left[counter].x-L) <= beta:
-            M.append(left[counter])
-            counter -= 1
-            if(counter < 0):
-                leftside = False
-        else:
-            leftside = False
-    counter = 0
-    while rightside == True:
-        if abs(right[counter].x - L) <= beta:
-            M.append(right[counter])
-            counter += 1
-            if(counter) == len(right):
-                rightside = False
-        else:
-            rightside = False
-    sortedM = MergeSort(M, 'y')
- #   minWithinL = []
-    for i in range(0, len(sortedM)):
-        for j in range(i, len(sortedM)):
+
+    for n in range(0, len(arrayy)):
+        if abs(arrayy[n].x-L) <= beta:
+            M.append(arrayy[n])
+
+
+    for i in range(0, len(M)):
+        for j in range(i, len(M)):
             if not(i == j):
-                if distance(sortedM[i], sortedM[j]) < beta:
+                if distance(M[i], M[j]) < distance(min[0], min[1]):
                     min[:] = []
-                    min.append(sortedM[i])
-                    min.append(sortedM[j])
+                    min.append(M[i])
+                    min.append(M[j])
 
-
-                if distance(sortedM[i], sortedM[j]) == beta:
-                    min.append(sortedM[i])
-                    min.append(sortedM[j])
-                if distance(sortedM[i], sortedM[j]) > beta:
+                if distance(M[i], M[j]) == distance(min[0], min[1]):
+                    if not(containsPair(min, M[i], M[j])):
+                        min.append(M[i])
+                        min.append(M[j])
+                        print("------- mins")
+                        for n in range(0, len(min)):
+                            print(min[n].x, min[n].y)
+                        print("------- mins")
+                if distance(M[i], M[j]) > beta:
                     break
 
     return min
@@ -219,8 +240,11 @@ def findSmallest(array):
     d = float
     minimum = 100000000000000000
     for f in range(0, len(array)):
+
         for s in range(f, len(array)):
+
             if (not (f == s)):
+
                 d = distance(array[f], array[s])
                 if d == minimum:
                     arrayOfMins.append(array[f])
@@ -234,41 +258,107 @@ def findSmallest(array):
 
     return arrayOfMins
 
-def closestPair(array):
-
+def closestPair(arrayx, arrayy):
+  #  array = MergeSort(array, 'x')
     min = []
-    left = []
-    right = []
+    leftx = []
+    rightx = []
+    lefty = []
+    righty = []
     minleft = []
     minright = []
     minimum = []
-    if (len(array) <= 3):
-        minimum = findSmallest(array)
+    if (len(arrayx) <= 3):
+
+        minimum = findSmallest(arrayx)
     else:
-        mid = int(len(array) / 2)
+        midx = int(len(arrayx) / 2)
+        midy = int(len(arrayy)/2)
 
-        for n in range(0, mid):
-            left.append(array[n])
+        for n in range(0, midx):
+            leftx.append(arrayx[n])
 
-        for n in range(mid, len(array)):
-            right.append(array[n])
-        minright = closestPair(right)
-        minleft = closestPair(left)
+        for n in range(midx, len(arrayx)):
+            rightx.append(arrayx[n])
+        middlexvalue = arrayx[midx].x
+
+        for n in range(0, len(arrayy)):
+            if(arrayy[n].x<= middlexvalue):
+                lefty.append(array[n])
+            else:
+                righty.append(array[n])
+        # the above for loop is step 2 in email
+
+        minright = closestPair(rightx, righty)
+        minleft = closestPair(leftx, lefty)
 
         disleft = distance(minleft[0], minleft[1])
         disright = distance(minright[0], minright[1])
 
         if len(min)>0:
+            if(disleft< disright):
+                if(disleft==distance(min[0], min[1] )):
+                    for m in range(0, len(minleft)):
+                        if (not (containsPair(min, minleft[m], minleft[m+1]))):
+                            min.append(minleft[m])
+                            min.append(minleft[m+1])
+                            m+=1
+                        m+=1
+                if disleft < distance(min[0], min[1]):
+                    min[:] = []
+                    min = minleft
+
+            if(disleft > disright):
+                if (disright == distance(min[0], min[1])):
+                    for m in range(0, len(minright)):
+                        if (not (containsPair(min, minright[m], minright[m + 1]))):
+                            min.append(minright[m])
+                            min.append(minright[m + 1])
+                            m += 1
+                        m+=1
+                if disright< distance(min[0], min[1]):
+                    min[:] = []
+                    min = minright
+            if(disleft == disright):
+                if (disright == distance(min[0], min[1])):
+                    for m in range(0, len(minright)):
+                        if (not (containsPair(min, minright[m], minright[m + 1]))):
+                            min.append(minright[m])
+                            min.append(minright[m + 1])
+                            m += 1
+                        m+=1
+                if disright< distance(min[0], min[1]):
+                    min = minright
+                if (disleft == distance(min[0], min[1])):
+                    for m in range(0, len(minleft)):
+                        if (not (containsPair(min, minleft[m], minleft[m + 1]))):
+                            min.append(minleft[m])
+                            min.append(minleft[m + 1])
+                            m += 1
+                        m += 1
+                if disleft< distance(min[0], min[1]):
+                    min[:] = []
+                    min = minleft
+
             if disleft == distance(min[0], min[1]):
                 for m in range(0, len(minleft)):
-                    min.append(minleft[m])
-            if disright == distance(min[0], min[1]):
-                for m in range(0, len(minright)):
-                    min.append(minright[m])
-            if disleft < distance(min[0], min[1]):
-                min = minleft
-            if disright <distance(min[0], min[1]):
-                min = minright
+                    if (not (containsPair(min, minleft[m], minleft[m+1]))):
+                        min.append(minleft[m])
+                        min.append(minleft[m+1])
+                        m+=1
+                    m+=1
+
+            # if disright == distance(min[0], min[1]):
+            #     for m in range(0, len(minright)):
+            #         if (not(containsPair(min, minright[m], minright[m+1]))):
+            #             min.append(minright[m])
+            #             min.append(minright[m+1])
+            #             m+=1
+            #
+            # if disleft < distance(min[0], min[1]):
+            #     min = minleft
+            # if disright <distance(min[0], min[1]):
+            #     min = minright
         else:
 
             if disleft < disright:
@@ -280,13 +370,15 @@ def closestPair(array):
                     min.append(minleft[n])
                 for n in range(0, len(minright)):
                     min.append(minright[n])
-        minimum = find_M_andComputeSmallestPair(left,right, min)
+        minimum = find_M_andComputeSmallestPair(leftx,rightx, min, arrayy)
 
     return minimum
 
-array = MergeSort(array, 'x')
-arrayOfMins = closestPair(array)
-file = open("Naive_output.txt","w")
+arrayx = MergeSort(array, 'x')
+arrayy = MergeSort(array,'y')
+arrayOfMins = closestPair(arrayx,arrayy)
+
+file = open("Enhanced_output.txt","w")
 distance1 = format(distance(arrayOfMins[0], arrayOfMins[1]))
 distance1+='\n'
 file.write(distance1)
@@ -302,9 +394,18 @@ for n in range(0, len(arrayOfMins)):
 
 print(distance(arrayOfMins[0], arrayOfMins[1]))
 
+
+
+
+
+
 for n in range(0, len(arrayOfMins)):
     print(arrayOfMins[n].x, arrayOfMins[n].y)
 
-
-
-
+# p1 = Point(1,2)
+# p2 = Point(3,4)
+# p3 = Point(5,6)
+# p4 = Point(7,8)
+# arraya = [Point(7,8),Point(7,8), Point(3,4),Point(1,2)]
+# if(containsPair(arraya, p1,p2)):
+#     print("contains")
